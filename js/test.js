3a38a789ec4b27545060a07fcaff9ab87441ba6c
BuildTools <unconfigured@null.spigotmc.org>
2020-11-10 15:51:25 +0000

    Bætti við linkum og leið til að reikna duration

--- js/test.js
+++ js/test.js
@@ -1,5 +1,6 @@
 var time = new Date().getTime();
 var years, months, weeks, days, hours;
+var minutes, seconds;
 fetch("./videos.json")
   .then(response => response.json())
   .then(data => {
@@ -9,22 +10,30 @@ fetch("./videos.json")
       <div class="row"></div>
       </div>`;
       for(var u = 0; u < Object.keys(data.categories[i].videos).length; u++) {
-        years = months = weeks = days = hours = 0;
+        years = months = weeks = days = hours = minutes = 0;
+        seconds = data.videos[data.categories[i].videos[u] - 1].duration;
         var timeCreated = time - data.videos[data.categories[i].videos[u] - 1].created;
-        if (timeCreated / 3.154e+10 > 1) {
+        if (timeCreated / 3.154e+10 >= 1) {
           years = Math.floor(timeCreated / 3.154e+10);
-        } else if (timeCreated / 2.628e+9 > 1) {
+        } else if (timeCreated / 2.628e+9 >= 1) {
           months = Math.floor(timeCreated / 2.628e+9);
-        } else if (timeCreated / 6.048e+8 > 1) {
+        } else if (timeCreated / 6.048e+8 >= 1) {
           weeks = Math.floor(timeCreated / 6.048e+8);
-        } else if (timeCreated / 8.64e+7 > 1) {
+        } else if (timeCreated / 8.64e+7 >= 1) {
           days = Math.floor(timeCreated / 8.64e+7)
         } else {
           hours = Math.floor(timeCreated / 3.6e+6 )
         }
+        if(seconds / 60 >= 1) {
+          minutes = Math.floor(seconds / 60);
+          seconds = seconds - 60 * minutes;
+        }
         document.querySelector(`.${data.categories[i].title.substr(0, data.categories[i].title.indexOf(" "))} .row`).innerHTML += `
-        <div class="myndband${data.videos[data.categories[i].videos[u] - 1].id} col col-4 col-sm-12 myndband">
-          <div class="myndband-poster"><img src="${data.videos[data.categories[i].videos[u] - 1].poster}"></div>
+        <a href="pages/myndband${data.videos[data.categories[i].videos[u] - 1].id}.html"><div class="myndband${data.videos[data.categories[i].videos[u] - 1].id} col col-4 col-sm-12 myndband">
+          <div class="myndband-poster">
+            <img src="${data.videos[data.categories[i].videos[u] - 1].poster}">
+            <div class="myndband-poster-time"><p>${minutes + ':' + ('0' + seconds).slice(-2)}</p></div
+            </div>
           <div class="myndband-title"><p>${data.videos[data.categories[i].videos[u] - 1].title}</p></div>
           <div class="myndband-time">${(() => {
             if(years != 0){
@@ -59,7 +68,7 @@ fetch("./videos.json")
               }
             }
           })()}</div>
-        </div>
+        </div></a>
         `;
       }
     }
