6f05ad4bf908be2925558be94d067567ffc2b5e8
BuildTools <unconfigured@null.spigotmc.org>
2020-11-10 04:33:08 +0000

    Upfærði tól og bætti við viku reikningi í js kóðan

--- js/test.js
+++ js/test.js
@@ -1,20 +1,22 @@
 var time = new Date().getTime();
-var years, months, days, hours
+var years, months, weeks, days, hours;
 fetch("./videos.json")
   .then(response => response.json())
   .then(data => {
-    for(i = 0; i < Object.keys(data.categories).length; i++) {
+    for(var i = 0; i < Object.keys(data.categories).length; i++) {
       document.querySelector("main").innerHTML += `<div class="${data.categories[i].title}">
       <h2>${data.categories[i].title}</h2>
       <div class="row"></div>
       </div>`;
-      for(u = 0; u < Object.keys(data.categories[i].videos).length; u++) {
-        years = months = days = hours = 0;
+      for(var u = 0; u < Object.keys(data.categories[i].videos).length; u++) {
+        years = months = weeks = days = hours = 0;
         var timeCreated = time - data.videos[data.categories[i].videos[u] - 1].created;
         if (timeCreated / 3.154e+10 > 1) {
           years = Math.floor(timeCreated / 3.154e+10);
         } else if (timeCreated / 2.628e+9 > 1) {
           months = Math.floor(timeCreated / 2.628e+9);
+        } else if (timeCreated / 6.048e+8 > 1) {
+          weeks = Math.floor(timeCreated / 6.048e+8);
         } else if (timeCreated / 8.64e+7 > 1) {
           days = Math.floor(timeCreated / 8.64e+7)
         } else {
@@ -36,6 +38,12 @@ fetch("./videos.json")
                 return `Fyrir 1 mánuði síðan`
               } else {
                 return `Fyrir ${months} mánuðum síðan`
+              }
+            } else if(weeks != 0) {
+              if(weeks == 1) {
+                return `Fyrir 1 viku síðan`
+              } else {
+                return `Fyrir ${weeks} vikum síðan`
               }
             } else if (days != 0) {
               if(days == 1) {
