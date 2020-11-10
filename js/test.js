666eda18153c254389593e66a75c852434fa53a8
BuildTools <unconfigured@null.spigotmc.org>
2020-11-10 03:54:17 +0000

    First javascript index.html code

---
+++ js/test.js
@@ -0,0 +1,59 @@
+var time = new Date().getTime();
+var years, months, days, hours
+fetch("./videos.json")
+  .then(response => response.json())
+  .then(data => {
+    for(i = 0; i < Object.keys(data.categories).length; i++) {
+      document.querySelector("main").innerHTML += `<div class="${data.categories[i].title}">
+      <h2>${data.categories[i].title}</h2>
+      <div class="row"></div>
+      </div>`;
+      for(u = 0; u < Object.keys(data.categories[i].videos).length; u++) {
+        years = months = days = hours = 0;
+        var timeCreated = time - data.videos[data.categories[i].videos[u] - 1].created;
+        if (timeCreated / 3.154e+10 > 1) {
+          years = Math.floor(timeCreated / 3.154e+10);
+        } else if (timeCreated / 2.628e+9 > 1) {
+          months = Math.floor(timeCreated / 2.628e+9);
+        } else if (timeCreated / 8.64e+7 > 1) {
+          days = Math.floor(timeCreated / 8.64e+7)
+        } else {
+          hours = Math.floor(timeCreated / 3.6e+6 )
+        }
+        document.querySelector(`.${data.categories[i].title.substr(0, data.categories[i].title.indexOf(" "))} .row`).innerHTML += `
+        <div class="myndband${data.videos[data.categories[i].videos[u] - 1].id} col col-4 myndband">
+          <div class="myndband-poster"><img src="${data.videos[data.categories[i].videos[u] - 1].poster}"></div>
+          <div class="myndband-title"><p>${data.videos[data.categories[i].videos[u] - 1].title}</p></div>
+          <div class="myndband-time">${(() => {
+            if(years != 0){
+              if(years == 1) {
+                return `Fyrir 1 ári síðan`
+              } else {
+                return `Fyrir ${years} árum síðan`
+              }
+            } else if (months != 0) {
+              if(months == 1) {
+                return `Fyrir 1 mánuði síðan`
+              } else {
+                return `Fyrir ${months} mánuðum síðan`
+              }
+            } else if (days != 0) {
+              if(days == 1) {
+                return `Fyrir 1 degi síðan`
+              } else {
+                return `Fyrir ${days} dögum síðan`
+              }
+            } else {
+              if(hours == 1) {
+                return `Fyrir 1 klukkutíma síðan`
+              } else {
+                return `Fyrir ${hours} klukkustundum síðan`
+              }
+            }
+          })()}</div>
+        </div>
+        `;
+      }
+    }
+    
+  });
